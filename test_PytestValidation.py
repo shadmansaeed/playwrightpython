5ab447ae65c3d34d035857df15135192b8ea3fb5
shadmansaeed <shadmansaeedjibon@gmail.com>
2025-09-27 02:31:29 +0600

    26.09.2025

--- test_PytestValidation.py
+++ test_PytestValidation.py
@@ -5,11 +5,20 @@ import pytest
 @pytest.fixture(scope="module")  # scope= 'module' dile sudhu 1 bar firste eita run hobe r function dile jotobar linkage hobe totobar run hobe
 def preWork():
     print("I setup module instance")
+    return "pass"
 
 
-def test_initialCheck(preWork):
+@pytest.fixture(scope="function")  # scope= 'module' dile sudhu 1 bar firste eita run hobe r function dile jotobar linkage hobe totobar run hobe
+def secondWork():
+    print("I setup secondWork instance")
+    yield  # pause
+    print("tear down validation")
+
+
+def test_initialCheck(preWork, secondWork):
     print("This is first test")
+    assert preWork == "pass"
 
 
-def test_SecondCheck(preSetupWork):
+def test_SecondCheck(preSetupWork, secondWork):
     print("This is second test")
