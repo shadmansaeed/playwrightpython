74d2502e21e9df87f385a443a15561fc8bcdd699
shadmansaeed <shadmansaeedjibon@gmail.com>
2025-10-10 01:45:09 +0600

    08.10.2025

--- playwright/test_MoreValidations.py
+++ playwright/test_MoreValidations.py
@@ -10,7 +10,19 @@ def test_UIChecks(page: Page):
     time.sleep(2)
     page.get_by_role("button", name="Hide").click()
     expect(page.get_by_placeholder("Hide/Show Example")).to_be_hidden()
-    time.sleep(5)
+    time.sleep(2)
+
+
+
+    #AlertBoxes
+    page.on("dialog", lambda dialog: dialog.accept())  # dialog box asle jeno accept korte pare eijonno use kora hoise
+    page.get_by_role("button", name="Confirm").click()  # oneliner function(anonymous functions are used as lambda)
+    time.sleep(3)
+
+    # FrameHandling
+    pageFrame = page.frame_locator("#courses-iframe")
+    pageFrame.get_by_role("link", name="All Access plan").click()
+    expect(pageFrame.locator("body")).to_contain_text("Happy Subscibers!")
 
 
 
